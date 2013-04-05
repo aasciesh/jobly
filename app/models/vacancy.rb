041f9e1908627674196abc4bd41249d48dedddfb
asish <asish@asish-VirtualBox.(none)>
2013-04-06 01:50:11 +0300

    added additional unit tests for vacancy model validation

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,5 +1,7 @@
 class Vacancy < ActiveRecord::Base
-  attr_accessible :city, :country, :deadline, :description, :job_address, :job_type, :latitude, :longitude, :street, :title, :zip
+  attr_accessible :city, :country, :deadline, :description, :job_address, 
+  					:job_type, :latitude, :longitude, :street, :title, :zip, 
+					:min_salary, :max_salary
 
   belongs_to :category
   belongs_to :company_profile
@@ -7,19 +9,22 @@ class Vacancy < ActiveRecord::Base
   has_many :tag_matches
   has_many :tags, through: :tag_matches
 
-  JOB_TYPES = ['fulltime','part time','traineeship','fixed term']
-
+  JOB_TYPES = ['fulltime','part-time','traineeship','fixed-term']
+  
   validates :title, presence: true, length: {minimum: 5, maximum: 150}
   validates :description, presence: true, length: {minimum:10, maximum: 3000}
-  validates	:deadline, presence: true
   validate :is_deadline_after_creation_date?
-  validates :job_type, presence: true, inclusion: {in: JOB_TYPES}
+  validates :job_type, presence: true, inclusion: {in: JOB_TYPES, message: '%{attribute} must be either fulltime part-time traineeship or fixed-term'}
   validates :job_address, presence: true, length: {maximum: 200}
+  validates :min_salary, numericality: true
+  validates :max_salary, numericality: true
+  validates	:deadline, presence: true
 
   def is_deadline_after_creation_date?
   	if self.deadline > DateTime.now
   		return true
   	else
+  		errors.add(:deadline, "must be after creation date.")
   		return false
   	end
   end
--- test/unit/vacancy_test.rb
+++ test/unit/vacancy_test.rb
@@ -4,14 +4,14 @@ require 'deadline_after_creation_date_validator'
 class VacancyTest < ActiveSupport::TestCase
   
   test "Vacancy should not save if Title not present" do
-  	vacancy = Vacancy.new
+  	vacancy = vacancies(:one)
   	vacancy.deadline = DateTime.now + 1.day
   	vacancy.title = ""
     assert vacancy.invalid?
     assert !vacancy.save
   end
   test "Title characters should not exceed total length of 150" do
-  	vacancy = Vacancy.new
+  	vacancy = vacancies(:one)
   	vacancy.deadline = DateTime.now + 1.day
   	vacancy.title = "This is test. This is test. This is test. This is test. This is test.
   						This is test.This is test.This is test.This is test.This is test.
@@ -21,14 +21,21 @@ class VacancyTest < ActiveSupport::TestCase
   end
 
   test "Deadline cannot be before or the same day as vacancy post date" do
-  	vacancy = Vacancy.new
+  	vacancy = vacancies(:one)
   	vacancy.deadline = DateTime.now - 1.day
   	assert vacancy.invalid?
   	assert !vacancy.save
   end
 
+  test "Deadline valid if it is later than creation date" do
+  	vacancy = vacancies(:one)
+  	vacancy.deadline = DateTime.now + 5.days
+  	assert vacancy.valid?
+  	assert vacancy.save
+  end
+
   test "Job address must be present" do 
-  	vacancy= Vacancy.new
+  	vacancy = vacancies(:one)
   	vacancy.deadline = DateTime.now + 1.day
   	vacancy.job_address = ""
   	assert vacancy.invalid?
@@ -36,12 +43,34 @@ class VacancyTest < ActiveSupport::TestCase
   end
 
   test "description must be more than 10 characters" do
-  	vacancy = Vacancy.new
+  	vacancy = vacancies(:one)
   	vacancy.deadline = DateTime.now + 1.day
   	vacancy.description = "hello"
   	assert vacancy.invalid?
   	assert !vacancy.save
   end
 
+  test "job types must be either fulltime part time traineeship fixed term" do
+  	vacancy = vacancies(:one)
+  	vacancy.deadline = DateTime.now + 1.day
+  	vacancy.job_type = "fulltime"
+  	assert vacancy.valid?
+  	assert vacancy.save
+  end
 
+  test "salary must not save if not numbers" do
+  	vacancy = vacancies(:one)
+  	vacancy.deadline = DateTime.now + 1.day
+  	vacancy.min_salary = "one thousand"
+  	assert vacancy.invalid?
+  	assert !vacancy.save
+  end
+
+  test "salary is saved if in numbers" do
+  	vacancy = vacancies(:one)
+  	vacancy.deadline = DateTime.now + 1.day
+  	vacancy.max_salary = 2000
+  	assert vacancy.valid?
+  	assert vacancy.save
+  end
 end
