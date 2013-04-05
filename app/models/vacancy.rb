3bed4c0edb2412c01abc45d8c43676c0399f4c02
asish <asish@asish-VirtualBox.(none)>
2013-04-06 00:44:22 +0300

    Vacancy unit tests

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -6,4 +6,21 @@ class Vacancy < ActiveRecord::Base
   has_many :applications, foreign_key: 'vacancy_id'
   has_many :tag_matches
   has_many :tags, through: :tag_matches
+
+  JOB_TYPES = ['fulltime','part time','traineeship','fixed term']
+
+  validates :title, presence: true, length: {minimum: 5, maximum: 150}
+  validates :description, presence: true, length: {minimum:10, maximum: 3000}
+  validates	:deadline, presence: true
+  validate :is_deadline_after_creation_date?
+  validates :job_type, presence: true, inclusion: {in: JOB_TYPES}
+  validates :job_address, presence: true, length: {maximum: 200}
+
+  def is_deadline_after_creation_date?
+  	if self.deadline > DateTime.now
+  		return true
+  	else
+  		return false
+  	end
+  end
 end
--- test/unit/vacancy_test.rb
+++ test/unit/vacancy_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
+require 'deadline_after_creation_date_validator'
 
 class VacancyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "Vacancy should not save if Title not present" do
+  	vacancy = Vacancy.new
+  	vacancy.deadline = DateTime.now + 1.day
+  	vacancy.title = ""
+    assert vacancy.invalid?
+    assert !vacancy.save
+  end
+  test "Title characters should not exceed total length of 150" do
+  	vacancy = Vacancy.new
+  	vacancy.deadline = DateTime.now + 1.day
+  	vacancy.title = "This is test. This is test. This is test. This is test. This is test.
+  						This is test.This is test.This is test.This is test.This is test.
+  						This is test.This is test.This is test.This is test.This is test."
+    assert vacancy.invalid?
+    assert !vacancy.save
+  end
+
+  test "Deadline cannot be before or the same day as vacancy post date" do
+  	vacancy = Vacancy.new
+  	vacancy.deadline = DateTime.now - 1.day
+  	assert vacancy.invalid?
+  	assert !vacancy.save
+  end
+
+  test "Job address must be present" do 
+  	vacancy= Vacancy.new
+  	vacancy.deadline = DateTime.now + 1.day
+  	vacancy.job_address = ""
+  	assert vacancy.invalid?
+  	assert !vacancy.save
+  end
+
+  test "description must be more than 10 characters" do
+  	vacancy = Vacancy.new
+  	vacancy.deadline = DateTime.now + 1.day
+  	vacancy.description = "hello"
+  	assert vacancy.invalid?
+  	assert !vacancy.save
+  end
+
+
 end
