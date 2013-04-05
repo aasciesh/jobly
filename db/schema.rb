d4585a9560438cc1f7ddae6d82b7e2c89831129a
asish <asish@asish-VirtualBox.(none)>
2013-04-06 00:49:29 +0300

    Added salary columns to Vacancies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130405095331) do
+ActiveRecord::Schema.define(:version => 20130405214808) do
 
   create_table "applications", :force => true do |t|
     t.integer  "user_id"
@@ -157,6 +157,8 @@ ActiveRecord::Schema.define(:version => 20130405095331) do
     t.float    "longitude"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "min_salary"
+    t.integer  "max_salary"
   end
 
 end
