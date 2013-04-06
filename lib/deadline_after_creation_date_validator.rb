d4638c78be3d7683067be5e650df002bfba20497
asish <asish@asish-VirtualBox.(none)>
2013-04-06 14:48:20 +0300

    vacancy custom validator removal again

--- lib/deadline_after_creation_date_validator.rb
+++
@@ -1,7 +0,0 @@
-class DeadlineAfterCreationDateValidator < ActiveModel::EachValidator
-  def validate_each(object, attribute, value)
-    unless Time.now < value
-      object.errors[attribute] << (options[:message] || "must be date after creation date.") 
-    end
-  end
-end
