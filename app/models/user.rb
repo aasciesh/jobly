4290c3adaa77a19b4a287241582787e5fbf47afa
Sigdel <sigdel@ubuntu.(none)>
2013-04-06 19:44:11 +0300

    added user_type

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation, :rememeber_cookie
+  attr_accessible :email, :password, :password_confirmation, :rememeber_cookie, :user_type
 
   has_secure_password
   belongs_to :profile, :polymorphic => true
+
+  USER_TYPES =['c','u']
+
+  validates_presence_of :email
+  validates_uniqueness_of :email  
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i 
+  validates_length_of :password, minimum: 8
+  validates_length_of :password, maximum: 20
+  validates_presence_of :user_type
+  validates_presence_of :password_confirmation
+  
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,99 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save only with valid params" do 
+  		user = User.new 	
+    	user.email = "a@xyz.com"
+    	user.password = "mypassword"
+    	user.type ="u"
+    	user.password_confirmation = "mypassword"
+    	assert user.valid?, "#{user.errors.messages}"
+    	assert user.save
+  end
+
+  test "Should not save without Email" do
+  		user = User.new 		
+    	user.password = "mypassword"
+    	user.password_confirmation = "mypassword"
+    	user.email = ""
+    	assert user.invalid?
+    	assert !user.save
+	end 
+
+	test "Invalid Email should not be saved" do 
+		user = User.new		
+    	user.password = "mypassword"
+    	user.password_confirmation = "mypassword"
+    	user.email = "example.com"
+    	assert user.invalid?
+    	assert !user.save
+	end 
+
+	test "Should not save without Password" do 
+		user = User.new
+		user.password = ""    	
+    	user.email = "a@xyz.com"
+    	assert user.invalid?
+    	assert !user.save
+	end 	
+
+	test "Email should be unique in user table" do 
+		user = User.new(email: "a@sigdel.com", 
+                    password: "mypassword", 
+                    password_confirmation: "mypassword")
+    	user.save
+
+    	user2 = User.new(email: user.email, 
+                     password: "mypassword", 
+                     password_confirmation: "mypassword")
+    	assert user2.invalid?
+    	assert !user2.save
+	end 
+
+	
+	test "Password and Password confirmation should be made" do 
+		user = User.new
+		user.password = "mypassword"
+		user.email = "a@sigdel.com"
+		assert user.invalid?
+		assert !user.save
+	end 
+
+	 test "user password must be between 8 and 20 characters" do
+    	user = User.new(:email => "a@sigdel.com")
+    	
+       	user.password = "123"    	
+    	assert user.invalid?
+    	assert !user.save
+
+    	user.password = ""    	
+    	assert user.invalid?
+    	assert !user.save
+
+    	#8 characters.
+    	user.password = "password"
+    	user.password_confirmation = "password"
+   	    assert user.valid?
+   	    assert !user.save
+
+        #20 characters.
+    	user.password = "12345678901234567890"
+    	user.password_confirmation = "12345678901234567890"
+    	assert user.valid?
+    	assert !user.save
+
+    	#21 characters.
+    	user.password = "123456789012345678901"    	
+    	assert user.invalid?
+    	assert !user.save
+    end	
+    test "User type must be either 'u' or 'c'." do
+    	user = User.new
+		user.password = "mypassword"
+		user.email = "a@sigdel.com"
+		user.user_type = "u"
+		assert user.invalid?
+		assert !user.save
+	end
+
 end
