ddce121f461feb35c1c4ebb43af43321fdde8cc8
Sigdel <sigdel@ubuntu.(none)>
2013-04-06 21:01:22 +0300

    validations made in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   USER_TYPES =['c','u']
 
   validates :email, presence: true 
-  validates_uniqueness_of :email  
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i 
+  validates :email, uniqueness: true 
+  validates :email, presence: true, format: { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i } 
   validates :password, presence: true, length: {minimum: 8, maximum: 20}
   validates :user_type, presence: true, inclusion: {in: USER_TYPES, message: 'User type must be c or u'}
   validates :password_confirmation, presence: true 
