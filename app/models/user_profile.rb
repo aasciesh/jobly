643ee8eaed110482dbee2df6296bc31de17fffd2
suresh pun <punsuresh@hotmail.com>
2013-04-19 17:31:46 +0300

    user_profile model validation with unit testing done

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -14,4 +14,27 @@ class UserProfile < ActiveRecord::Base
   has_many :language_skills
   has_many :references
 
+USER_GENDER_TYPE= ['m','f']
+
+  validates :firstname, presence: true, length: {minimum: 2, maximum: 50}
+  validates :lastname, presence: true, length: {minimum: 2, maximum: 50}
+  validates :country, presence: true, length: {minimum: 2, maximum: 50, message: 'Give country name'}
+  validate :is_birth_date_given_and_is_above_16?
+  # validates :birth_date, presence: true, message: 'birth date above 16'
+  validates :gender, presence:true, inclusion: {in: USER_GENDER_TYPE, message: 'User type must be m or f'}
+  validates :full_address, presence: true, length: {minimum: 2, maximum: 250}
+
+  private
+
+  def is_birth_date_given_and_is_above_16?
+    if self.birth_date.nil?
+        errors.add(:birth_date, "must be given")
+        return false
+    elsif self.birth_date <= (DateTime.now-16.years)
+        return true
+    elsif self.birth_date > (DateTime.now-16.years)
+      errors.add(:birth_date, "must be atleast 16.")
+      return false
+    end
+  end
 end
--- test/unit/user_profile_test.rb
+++ test/unit/user_profile_test.rb
@@ -1,7 +1,100 @@
 require 'test_helper'
 
+
 class UserProfileTest < ActiveSupport::TestCase
+  test "should not save without firstname and lastname" do
+  userprofile=user_profiles(:one)
+  userprofile=user_profiles(:one)
+      # userprofile=UserProfile.new
+      # userprofile=UserProfile.new
+  userprofile.lastname=""
+  userprofile.firstname=""
+  assert !userprofile.valid?
+  assert !userprofile.save
+  #	 assert_not_equal(1, 2)
+  end
+
+
+   test "should not save until gender is given" do
+   	 userprofile=user_profiles(:one)
+   	 userprofile.gender=''
+   	 assert !userprofile.valid?
+   	 assert !userprofile.save
+   end
+
+  # # test "should not save until street is given" do
+
+  # # end
+
+  # # test "should not save until city is given" do
+
+  # # end
+  # def setup
+
+  # end
+
+    test "should not save until full adress is given" do
+      userprofile=user_profiles(:one)
+      userprofile.full_address=''
+      assert !userprofile.valid?
+      assert !userprofile.save
+   end
+
+   test "should not save until country is given" do
+      userprofile=user_profiles(:one)
+      userprofile.country=''
+      assert !userprofile.valid?
+      assert !userprofile.save
+   end
+
+  # test "should not save until self_info is given" do
+  # userprofile=user_profiles(:one)
+  # userprofile.self_info="Write something about u"
+  # assert !userprofile.save
+ 
+  # end
+
+  # test "should not save until zip is given" do
+# userprofile=user_profiles(:one)
+  # userprofile.zip=''
+  # assert userprofile.invalid?
+  # assert !userprofile.save
+  #  end
+
+  test "should not save until birth_date is given" do
+      userprofile=  user_profiles(:one)
+      userprofile.birth_date='' 
+      puts 'true TRUE true' if !userprofile.nil?
+      puts userprofile.birth_date
+      assert userprofile.invalid?
+      assert !userprofile.save
+   end
+
+   test "should not save if user's age is less than 16 years" do
+      userprofile=  user_profiles(:one)
+      userprofile.birth_date= DateTime.now-2.years 
+      puts userprofile.birth_date
+      assert userprofile.invalid?
+      assert !userprofile.save
+   end
+
+  # test "should not save until hobby is given" do
+  # userprofile=user_profiles(:one)
+  # userprofile.hobbies="roaming, going to cruise"
+  # assert !userprofile.save
+  #  end
+
+  # test "should not save until fulladdress is given" do
+# userprofile=user_profiles(:one)
+  # userprofile.full_address="Give some full address"
+  # assert !userprofile.save
+  # end
+
+  # test "should not save until avatar is given" do
+
+  # end
   # test "the truth" do
   #   assert true
   # end
+
 end
