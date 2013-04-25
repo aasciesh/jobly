0184cb01ca5d60b3b0805c6cd5f819fd041858a5
Prakash Sigdel <Prakash.Sigdel@myy.haaga-helia.fi>
2013-04-25 20:46:03 +0300

    functional test and controllers for language_skills but not working

--- app/controllers/language_skills_controller.rb
+++ app/controllers/language_skills_controller.rb
@@ -1,3 +1,16 @@
 class LanguageSkillsController < ApplicationController
 	
+	def new
+    @user_profile.language_skill = UserProfile.LanguageSkill.new(name: 'english',
+  										level: '5',
+  										type: 'spoken')
+
+    @user_profile.language_skill.build_user
+
+    respond_to do |format|
+      format.html 
+      format.js
+    end
+
+    
 end
--- test/functional/language_skills_controller_test.rb
+++ test/functional/language_skills_controller_test.rb
@@ -10,34 +10,34 @@ class LanguageSkillsControllerTest < ActionController::TestCase
   	@current_user.language_skill	
   end
 
-  # test "should get show" do
-  #   get :show
-  #   assert_response :success
-  # end
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
 
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
 
-  # test "should get edit form" do
-  #   get :edit
-  #   assert_response :success
-  # end
+  test "should get edit form" do
+    get :edit
+    assert_response :success
+  end
 
-  # test "should get update" do
-  #   get :update
-  #   assert_response :success
-  # end
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
 
-  # test "should get destroy" do
-  #   get :destroy
-  #   assert_response :success
-  # end
+  test "should get destroy" do
+    get :destroy
+    assert_response :success
+  end
 
 end
