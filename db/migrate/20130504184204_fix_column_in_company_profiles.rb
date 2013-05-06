664d9785167ed1956bd8bd36a363d741d4f02ebd
Asish Bhattarai <getaasciesh@hotmail.com>
2013-05-06 20:06:52 +0300

    deleted fixCompanyProfileColumn

--- db/migrate/20130504184204_fix_column_in_company_profiles.rb
+++
@@ -1,6 +0,0 @@
-class FixColumnInCompanyProfiles < ActiveRecord::Migration
-  def change
-  	remove_column :company_profiles, :type
-  	add_column :company_profiles, :company_type, :string
-  end
-end
