2ef0dfe8af4c25bea69febb8c1d19670de60054a
sakomee <somee12@hotmail.com>
2013-05-09 14:59:38 +0300

    add precised and strick authorization via cancan

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ActiveRecord::Base
-  attr_accessible :user_id, :vacancy_id
+  attr_accessible :user_id, :vacancy_id, :bookmark
 
   belongs_to :user_profile
   belongs_to :vacancy
