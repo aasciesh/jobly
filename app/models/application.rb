5b205f6e2f730fd03163000d2d3bb677b2e6c6f9
sakomee <somee12@hotmail.com>
2013-05-12 17:01:03 +0300

    #Possibility to select references while applying to vacancy #Create, update and delete actions for reference made

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,10 @@
 class Application < ActiveRecord::Base
-  attr_accessible :user_id, :vacancy_id, :bookmark, :cover_letter
+  attr_accessible :user_id, :vacancy_id, :bookmark, :cover_letter, :reference_ids
 
   belongs_to :user_profile, foreign_key: :user_id
   belongs_to :vacancy
-  has_many :references
+  has_many :application_references
+  has_many :references, through: :application_references
 
   validates :user_id, presence: true
   validates :vacancy_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130511183800) do
+ActiveRecord::Schema.define(:version => 20130512083205) do
+
+  create_table "application_references", :force => true do |t|
+    t.integer  "application_id"
+    t.integer  "reference_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
   create_table "applications", :force => true do |t|
     t.integer  "user_id"
