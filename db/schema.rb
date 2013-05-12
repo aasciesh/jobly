2d9cd7d384a43ff16358c54b0b46e164d00b27d3
sakomee <somee12@hotmail.com>
2013-05-12 19:01:28 +0300

    applying to vacancy reloads page and show message now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130512083205) do
 
   create_table "application_references", :force => true do |t|
@@ -20,7 +19,6 @@ ActiveRecord::Schema.define(:version => 20130512083205) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
   end
-
 
   create_table "applications", :force => true do |t|
     t.integer  "user_id"
